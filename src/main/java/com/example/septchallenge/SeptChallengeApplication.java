6f7eeab5ac4e1cf3ffa2db333fb81d51ff1d4a4f
Yeuni <hinalee5250@gmail.com>
2022-09-09 10:35:11 +1000

    First Commit

---
+++ src/main/java/com/example/septchallenge/SeptChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.septchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeptChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeptChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/septchallenge/SeptChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.septchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
